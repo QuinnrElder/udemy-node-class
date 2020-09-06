8ac9c95d78ff79d57532f61f41bac18faf4bb394
Quinn Elder <qre.turing@gmail.com>
2020-09-06 15:13:28 -0600

    Create a Node.js Server

---
+++ app.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+  // console.log(req.url, req.method, req.headers)
+  const method = req.method
+  const url = req.url;
+  if(url === '/') {
+    res.write('<html>')
+    res.write('<header><title>Enter Message</title></header>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">SEND</button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync('message.txt', "DUMMY")
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end()
+  }
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<header><title>My First Page</title></header>')
+  res.write('<body><h1>Hello, welcome to my Node.js Server</h1></body>')
+  res.write('</html>')
+  res.end()
+});
+
+server.listen(3000)
