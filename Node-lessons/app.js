a520e6586385d3af9c851660c1b1d7be9abfb1d6
Quinn Elder <qre.turing@gmail.com>
2020-09-20 17:21:33 -0600

    Add dynamic routes

--- Node-lessons/app.js
+++ Node-lessons/app.js
@@ -1,3 +1,5 @@
+const path = require('path')
+
 const express = require('express')
 const app = express()
 
@@ -8,8 +10,11 @@ const shopRoutes = require("./routes/shop")
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
-
+app.use("/admin", adminRoutes)
 app.use(shopRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "error.html"))
+})
 
 app.listen(3000)
