9f2a835abe2b4ea3ec9336bf3b9d87abee161da6
Quinn Elder <qre.turing@gmail.com>
2020-09-23 12:13:24 -0600

    Refactor  .js files for new course module

--- Node-lessons/routes/admin.js
+++ Node-lessons/routes/admin.js
@@ -1,17 +1,23 @@
-const express = require("express")
-const path = require('path')
+const path = require('path');
 
-const rootDir = require("../util/path")
+const express = require('express');
 
-const router = express.Router()
+const rootDir = require('../util/path');
 
-router.get("/add-product", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "add-product.html"))
-})
+const router = express.Router();
 
-router.post("/add-product", (req, res, next) => {
-  console.log(req.body)
-  res.redirect("/")
-})
+const products = [];
 
-module.exports = router
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
--- Node-lessons/routes/shop.js
+++ Node-lessons/routes/shop.js
@@ -1,12 +1,15 @@
-const path = require('path')
-const express = require("express")
+const path = require('path');
 
-const rootDir = require("../util/path")
+const express = require('express');
 
-const router = express.Router()
+const rootDir = require('../util/path');
+const adminData = require('./admin');
 
-router.get("/", (req, res, next) => {
-  res.sendFile(path.join(rootDir, "views", "shop.html"))
-})
+const router = express.Router();
 
-module.exports = router
+router.get('/', (req, res, next) => {
+  console.log('shop.js', adminData.products);
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
